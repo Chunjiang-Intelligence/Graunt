f1871fbca7cfcb7f582ccd38195d5861197f471f
imbue-bit <imbue2025@outlook.com>
2026-02-25 07:22:06 +0800

    feat: 定义 RLHF 服务

---
+++ internal/service/rlhf.go
@@ -0,0 +1,23 @@
+package service
+
+import (
+	"data-flywheel/pkg/naivebayes"
+)
+
+type RLHFService struct {
+	Classifier *naivebayes.BayesClassifier
+}
+
+func NewRLHFService() *RLHFService {
+	return &RLHFService{
+		Classifier: naivebayes.NewBayesClassifier(),
+	}
+}
+
+func (s *RLHFService) SubmitKnownEvaluation(userID string, userEval, actualCorrect bool) {
+	s.Classifier.UpdateProfile(userID, userEval, actualCorrect)
+}
+
+func (s *RLHFService) InferUnknownEvaluation(userID string, userEval bool) float64 {
+	return s.Classifier.InferUnknownQuality(userID, userEval)
+}
